a5cf59fb45c0261f4aad53db9a6906293a867e8c
max3 <max3@rose-hulman.edu>
2018-03-12 09:03:33 -0400

    Done

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Xiaoyu Ma.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -95,8 +95,16 @@ def draw_parallel_lines(n, point, length, window):
       :type length: int
       :type window: rg.RoseWindow
     """
+    x = point.x
+    y = point.y
+
+    for k in range(n):
+        line = rg.Line(rg.Point(x, y), rg.Point(x + length, y))
+        line.attach_to(window)
+        window.render()
+        y += 30
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
@@ -160,8 +168,18 @@ def draw_lines(n, point, window):
       :type point: rg.Point
       :type window: rg.RoseWindow
     """
+    x = point.x
+    y = point.y
+    diff = ((y + 100) - (y - 100)) / (n - 1)
+    end = y - 100
+
+    for k in range(n):
+        line = rg.Line(rg.Point(x, y), rg.Point(x + 100, end))
+        line.attach_to(window)
+        window.render()
+        end += diff
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
